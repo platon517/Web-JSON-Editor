eb9fc4214095959aa1ed25230cf83e54bf426cf6
platon517 <36364651+platon517@users.noreply.github.com>
2018-03-10 20:49:01 +0300

    Add files via upload

--- docs/bundle.js
+++ docs/bundle.js
@@ -29442,7 +29442,7 @@ var OpenFile = exports.OpenFile = function (_React$Component) {
                     });
                 };
             } else {
-                console.log(file.type);
+                console.log(file);
                 alert("file is not .json");
                 this.props.cancelDrop();
                 this.setState({ isDragOver: false });
